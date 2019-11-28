e8c317fd13459b994164eac2e353e2d7d4a9a800
Nida759 <55382688+Nida759@users.noreply.github.com>
2019-11-28 11:47:57 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,14 +1,22 @@
-fn main() {
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
 
-    let r;
-
-    {
-        let x = 5;
-        r = &x;
-
-        println!("r: {}", r);
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
+}
 
-    //  println!("r: {}", r);
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
 
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
